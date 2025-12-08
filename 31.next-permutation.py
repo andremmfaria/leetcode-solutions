73f45e6aa6fa3ef44b40b95817e354ce6fc4257b
Andre Faria <andremarcalfaria@gmail.com>
2025-12-08 00:41:31 +0000

    solved for array and permutation

--- 31.next-permutation.py
+++ 31.next-permutation.py
@@ -3,13 +3,111 @@
 #
 # [31] Next Permutation
 #
+# https://leetcode.com/problems/next-permutation/description/
+#
+# algorithms
+# Medium (44.04%)
+# Likes:    20645
+# Dislikes: 5040
+# Total Accepted:    2M
+# Total Submissions: 4.6M
+# Testcase Example:  '[1,2,3]'
+#
+# A permutation of an array of integers is an arrangement of its members into a
+# sequence or linear order.
+# 
+# 
+# For example, for arr = [1,2,3], the following are all the permutations of
+# arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+# 
+# 
+# The next permutation of an array of integers is the next lexicographically
+# greater permutation of its integer. More formally, if all the permutations of
+# the array are sorted in one container according to their lexicographical
+# order, then the next permutation of that array is the permutation that
+# follows it in the sorted container. If such arrangement is not possible, the
+# array must be rearranged as the lowest possible order (i.e., sorted in
+# ascending order).
+# 
+# 
+# For example, the next permutation of arr = [1,2,3] is [1,3,2].
+# Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+# While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does
+# not have a lexicographical larger rearrangement.
+# 
+# 
+# Given an array of integers nums, find the next permutation of nums.
+# 
+# The replacement must be in place and use only constant extra memory.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [1,2,3]
+# Output: [1,3,2]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [3,2,1]
+# Output: [1,2,3]
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: nums = [1,1,5]
+# Output: [1,5,1]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 100
+# 0 <= nums[i] <= 100
+# 
+# 
+#
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+        Algorithm (O(n) time, O(1) space):
+        1) Find the first index i from the right where nums[i] < nums[i+1]. If none, reverse all.
+        2) From the right, find the first j such that nums[j] > nums[i], swap i and j.
+        3) Reverse the suffix starting at i+1.
         """
-        
+        n = len(nums)
+        if n <= 1:
+            return
+
+        # 1) Find pivot i
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            # 2) Find rightmost element greater than pivot, swap
+            j = n - 1
+            while j >= 0 and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # 3) Reverse suffix
+        left, right = i + 1, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
 # @lc code=end
 
